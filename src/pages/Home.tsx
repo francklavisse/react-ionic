9f44436166ac6094eb6d9d6953a8db62f3e56a9c
Franck Lavisse <lavisse.franck@gmail.com>
2019-12-06 22:14:01 +0100

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import React from 'react';
+import { useCamera, availableFeatures } from '@ionic/react-hooks/camera';
+import { CameraResultType } from '@capacitor/core'
 
 const Home: React.FC = () => {
+  const { photo, getPhoto} = useCamera();
+
+  const handleTakePhoto = () => {
+      if(availableFeatures.getPhoto) {
+        getPhoto({
+          quality: 100,
+          allowEditing: false,
+          resultType: CameraResultType.DataUrl
+        })
+      }
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -10,14 +23,12 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        The world is your oyster.
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
+        {availableFeatures.getPhoto ? (
+          <div>
+            <div><IonButton onClick={handleTakePhoto}>Take Photo</IonButton></div>
+            <div>{photo && <img alt="" src={photo.dataUrl} />}</div>
+          </div>
+        ) : <div>Camera not available on this platform</div>}
       </IonContent>
     </IonPage>
   );
